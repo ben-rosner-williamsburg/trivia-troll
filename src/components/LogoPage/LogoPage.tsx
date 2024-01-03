4af3cb9cd43892c0750471d9320a253239deb34a
Jen Nguyen <jenniferannnguyen@live.com>
2024-01-02 19:52:30 -0700

    Add styling to LogoPage

--- src/components/LogoPage/LogoPage.tsx
+++ src/components/LogoPage/LogoPage.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import './LogoPage.scss'
 import { Link } from "react-router-dom";
-
+// import HappyTroll from '../HappyTroll/HappyTroll'
+import HappyTroll from '../../Images/HappyTroll.png';
 const LogoPage = () => {
   return (
-    <div>
-      <Link to="/main">
-        <button className="enter-game-btn">Enter</button>
-      </Link>
-    </div>
+    <main className='logo-page'>
+      <div className='content-wrapper'>
+        <div className='image-wrapper'>
+          <img src={HappyTroll} alt='happy-troll' className='happy-troll-logo-page' />
+        </div>
+        <div className='text-wrapper'>
+          <h1 className='trivia-troll-logo-title'>Trivia Troll</h1>
+          <Link to='/main'>
+            <button className='enter-game-btn'>Enter</button>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 };
 
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,14 +1,17 @@
+import './MainPage.scss'
 import { Link } from 'react-router-dom';
 import DropDownMenu from '../DropDownMenu/DropDownMenu';
+import Header from '../Header/Header'
 
 const MainPage = ({ setSelectedDifficulty }: any) => {
   return (
-    <div>
+    <main className='main-page'>
+      <Header />
       <DropDownMenu setSelectedDifficulty={setSelectedDifficulty} />
       <Link to="/game">
         <button className="play-btn">Play Game</button>
       </Link>
-    </div>
+    </main>
   );
 };
 
