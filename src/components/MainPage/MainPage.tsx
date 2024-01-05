6d9d5c094c55b15c6b44c6c758c93015169dbfd2
William Lavery <lavery_william@yahoo.com>
2024-01-04 20:01:09 -0500

    Add styling to MainPage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -7,6 +7,7 @@ const MainPage = ({ setSelectedDifficulty }: any) => {
   return (
     <main className="main-page">
       <Header />
+      <h2>Enter the Trivia Troll's Lair if You Dare!</h2>
       <DropDownMenu setSelectedDifficulty={setSelectedDifficulty} />
       <Link to="/game">
         <button className="play-btn">Play Game</button>
