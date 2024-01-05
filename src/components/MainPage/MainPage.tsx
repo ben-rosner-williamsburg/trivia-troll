cc524d15fbbee70db4a9b500f9d94c261088800a
William Lavery <lavery_william@yahoo.com>
2024-01-04 21:02:07 -0500

    Add styling for MainPage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -7,11 +7,13 @@ const MainPage = ({ setSelectedDifficulty }: any) => {
   return (
     <main className="main-page">
       <Header />
-      <h2>Enter the Trivia Troll's Lair if You Dare!</h2>
-      <DropDownMenu setSelectedDifficulty={setSelectedDifficulty} />
-      <Link to="/game">
-        <button className="play-btn">Play Game</button>
-      </Link>
+      <h3>Enter the Trivia Troll's Lair if You Dare!</h3>
+      <div className="choose-and-play-menu">
+        <DropDownMenu className="select-level" setSelectedDifficulty={setSelectedDifficulty} />
+        <Link className="go-play" to="/game">
+          <button className="play-btn">Play Game</button>
+        </Link>
+      </div>
     </main>
   );
 };
