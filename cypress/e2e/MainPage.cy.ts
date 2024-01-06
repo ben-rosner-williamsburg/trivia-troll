e954ba3ce2649b3f089c0d95a0b7b32a01915a46
William Lavery <lavery_william@yahoo.com>
2024-01-06 09:46:28 -0500

    Add MainPage tests for dropdown, label and button

--- cypress/e2e/MainPage.cy.ts
+++ cypress/e2e/MainPage.cy.ts
@@ -16,10 +16,16 @@ describe('Main Page', () => {
   it('should display a subheading', () => {
     cy.get("h3").contains("Enter the Trivia Troll's Lair if You Dare!").should("be.visible");
   });
-  it.only("Validate the dropdown option selection by it's text", () => {
+  it("Validate the dropdown option selection by it's text", () => {
     cy.get("select")
     .select("Easy")
     .invoke("val")
     .should("eq", "easy")
+  });
+  it('should display a label for the dropdown menu', () => {
+    cy.get("label").contains("Select Difficulty").should("be.visible");
+  });
+  it('should display a play game button', () => {
+    cy.get("button").contains("Play Game").should("be.visible");
   });
 })
